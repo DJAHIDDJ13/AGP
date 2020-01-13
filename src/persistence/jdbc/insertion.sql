c03f02dcd781f7c79df67fb1f8c46d479186ef9d
Do <lay_donald@hotmail.fr>
2020-01-13 01:38:01 +0100

    Update SQL creation script + starting population script + add MCD

---
+++ src/persistence/jdbc/insertion.sql
@@ -0,0 +1,46 @@
+--------------------------------------------------
+-- Remplissage des tables de la base de données -- 
+--------------------------------------------------
+
+
+---------   Table Island  ---------  
+INSERT INTO island VALUES ('isl001','Praslin');
+INSERT INTO island VALUES ('isl002','La Digue');
+INSERT INTO island VALUES ('isl003','Ste Anne');
+INSERT INTO island VALUES ('isl004','Silhouette');
+INSERT INTO island VALUES ('isl005','Nord');
+INSERT INTO island VALUES ('isl006','Oiseaux');
+INSERT INTO island VALUES ('isl007','Curieuse');
+INSERT INTO island VALUES ('isl008','Eden Island');
+INSERT INTO island VALUES ('isl009','Cerf');
+INSERT INTO island VALUES ('isl010','Desroches');
+INSERT INTO island VALUES ('isl011','Denis');
+INSERT INTO island VALUES ('isl012','Assomption');
+INSERT INTO island VALUES ('isl013','Frégate');
+INSERT INTO island VALUES ('isl014','Saint-Pierre');
+INSERT INTO island VALUES ('isl015','Atoll Farquhar');
+INSERT INTO island VALUES ('isl016','Coetivy');
+INSERT INTO island VALUES ('isl017','Saint-Francois');
+INSERT INTO island VALUES ('isl018','D Arros');
+INSERT INTO island VALUES ('isl019','Felicite');
+INSERT INTO island VALUES ('isl020','Mahe');
+
+---------   Table Hotel  --------- 
+INSERT INTO hotel VALUES ('hot001','isl002','Six Senses Zil Pasyon', 5, 375); 
+INSERT INTO hotel VALUES ('hot002','isl020','Anse Soleil Beachcomber', 2, 100); 
+INSERT INTO hotel VALUES ('hot003','isl001','Hotel Le Duc de Praslin', 3, 186); 
+INSERT INTO hotel VALUES ('hot004','isl001','Coco de Mer Hotel and Black Parrot Suites', 4, 278); 
+INSERT INTO hotel VALUES ('hot005','isl001','Constance Lemuria Praslin Seychelles', 5, 666);
+INSERT INTO hotel VALUES ('hot006','isl011','Denis Private Island', 4, 1755);
+INSERT INTO hotel VALUES ('hot007','isl001','Paradise Sun', 4, 720);
+INSERT INTO hotel VALUES ('hot008','isl020','Carana Beach', 4, 364);
+INSERT INTO hotel VALUES ('hot009','isl009','Cerf Island Resort', 4, 413);
+INSERT INTO hotel VALUES ('hot010','isl020','Coral Strand Smart Choice', 4, 163);
+INSERT INTO hotel VALUES ('hot011','isl002','Le Nautique Luxury Waterfront', 3, 310);
+INSERT INTO hotel VALUES ('hot012','isl004','La Belle Tortue Lodge & Restaurant', 4, 425);
+INSERT INTO hotel VALUES ('hot013','isl001','Hotel Archipel', 4, 1264);
+INSERT INTO hotel VALUES ('hot014','isl020','Lazare Picault', 2, 122);
+INSERT INTO hotel VALUES ('hot015','isl008','Eden Bleu', 4, 282);
+INSERT INTO hotel VALUES ('hot01§','isl002','Patatran Village', 3, 380);
+INSERT INTO hotel VALUES ('hot017','isl008','Halets Cote Mer', 2, 130);
+INSERT INTO hotel VALUES ('hot018','isl020','Hilton Seychelles Northolme Resort & Spa', 5, 375);
