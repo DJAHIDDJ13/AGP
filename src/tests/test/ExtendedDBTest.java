b41317d9762378997e5691777ad3b4ea72f99107
ishak96 <ishakayad96@outlook.fr>
2020-01-16 22:46:34 +0100

    Add tests for BDE

--- src/tests/test/ExtendedDBTest.java
+++ src/tests/test/ExtendedDBTest.java
@@ -1,40 +1,46 @@
 package tests.test;
 
+import static org.junit.Assert.assertTrue;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
 import bde.api.ExtendedDB;
 import bde.iterator.Iterator;
 
 public class ExtendedDBTest {
 
-	public static void main(String[] args) {
-		
-		String table = "site";
-		String cle = "id_site";
-		String dataDir = "src/luceneSRC/data/";
-		
-		ExtendedDB bde = new ExtendedDB(table, cle, dataDir);
-		
+	private static ExtendedDB bde;
+	
+	private final static String table = "site";
+	private final static String key = "id_site";
+	private final static String dataDir = "src/luceneSRC/data/";
+	
+	private final static String luceneQuery = "baie OR culture";
+	private final static String sqlQuery = "SELECT id_site, type_site FROM site WHERE type_site = 'historic'";
+	
+	private final static String mixedQuery = "SELECT type_site FROM site WHERE type_site = 'historic' with baie OR culture";
+
+	private static long plan1Duration = 0;
+	private static long plan2Duration = 0;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		bde = new ExtendedDB(table, key, dataDir);
 		bde.createIndexDir();
-		
-		System.out.println("\nQUERY : ");
-		String query = "baie OR culture";
-		Iterator iterator = bde.luceneQuery(query);
-		
-		System.out.println("\n-------- Lucene Iterator result --------");
-		try {
-			while(iterator.next()) {
-				System.out.println("ID : " + iterator.getString(0) + ", Pertinance : " + iterator.getString(1));		
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
+	}
+	
+	@Test
+	public void testSQLOperator() {
 		System.out.println("\n-------- SQL JDBC Iterator result --------");
 		
-		query = "SELECT * FROM site WHERE type_site = 'historic'";
-		iterator = bde.SQLQuery(query);
+		Iterator iterator = bde.SQLQuery(sqlQuery);
 		
+		int sum = 0;
 		try {
 			while(iterator.next()) {
+				sum++;
 				for(int i = 1; i <= iterator.getColumnCount(); i++) {
 					System.out.print(iterator.getString(i) + " ");
 				}
@@ -42,13 +48,35 @@ public class ExtendedDBTest {
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
+		}		
+		
+		assertTrue(sum > 0);
+	}
+	
+	@Test
+	public void testLuceneOperator() {
+		
+		Iterator iterator = bde.luceneQuery(luceneQuery);
+		
+		System.out.println("\n-------- Lucene Iterator result --------");
+		int i = 0;
+		try {
+			while(iterator.next()) {
+				i++;
+				System.out.println("ID : " + iterator.getString(0) + ", Pertinance : " + iterator.getString(1));		
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
+		assertTrue(i > 0);
+	}
+
+	@Test
+	public void testPlaneNestedLoopJoin() {
+		
+		Iterator iterator = bde.MixedQuery(mixedQuery);
 		
 		System.out.println("\n-------- Plan 1 Nested Loop join Iterator result --------");
-		
-		//query = "SELECT * FROM site WHERE type_site = 'historic' with baie OR culture";
-		query = "SELECT type_site FROM site WHERE type_site = 'historic' with baie OR culture";
-		iterator = bde.MixedQuery(query);
 		
 		long startTime = System.nanoTime();
 		try {
@@ -63,12 +91,19 @@ public class ExtendedDBTest {
 		}
 		long stopTime = System.nanoTime();
 		
-		System.out.println("the time for the first plane is : " + (stopTime-startTime));
+		 plan1Duration = stopTime-startTime;
+		 
+		 assertTrue(plan1Duration > 0);
+	}
+
+	@Test
+	public void testPlaneIndexedJoin() {
+		
+		Iterator iterator = bde.MixedIndexedQuery(mixedQuery);
 		
 		System.out.println("\n-------- Plan 2 Indexed join Iterator result --------");
-		iterator = bde.MixedIndexedQuery(query);
 		
-		startTime = System.nanoTime();
+		long startTime = System.nanoTime();
 		try {
 			while(iterator.next()) {
 				for(int i = 1; i < iterator.getColumnCount(); i++) {
@@ -79,10 +114,20 @@ public class ExtendedDBTest {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}	
-		stopTime = System.nanoTime();
+		long stopTime = System.nanoTime();
 
-		System.out.println("the time for the first plane is : " + (stopTime-startTime));
+		plan2Duration = stopTime-startTime;	
 		
+		assertTrue(plan2Duration > 0);
+	}
+	
+	@Test
+	public void testPlaneCompare() {
+		assertTrue(plan2Duration > plan1Duration);
+	}	
+	
+    @AfterClass
+	public static void afterTesting() {
 		bde.close();
 	}
 }
